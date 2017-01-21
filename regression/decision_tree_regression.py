d0b35ee82661732d8137605b5a2e350ce959fadd
Jakub Piotrowski <piotrowski.jakub1993@gmail.com>
2017-01-21 21:25:08 +0100

    Implementation of SVR, Decision Tree Regression and Random Forest Regression for Facebook stock

--- regression/decision_tree_regression.py
+++ regression/decision_tree_regression.py
@@ -1,4 +1,4 @@
-# POLYNOMIAL REGRESSION
+# Decision Tree Regression
 
 # Importing the libraries
 import quandl
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 import math
 
 # Parameters
-polynomial_degree = 4
 test_size = 0.2
 quandl_dataset = "WIKI/FB"
 
@@ -27,34 +26,28 @@ y_train = y[0:split_index]
 X_test = X[split_index:, :]
 y_test = y[split_index:]
 
-# Fitting to the dataset
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import PolynomialFeatures
-poly_reg = PolynomialFeatures(degree = polynomial_degree)
-X_poly = poly_reg.fit_transform(X_train)
-regressor = LinearRegression()
-regressor.fit(X_poly, y_train)
+# Fitting Decision Tree Regression to the dataset
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor()
+regressor.fit(X_train, y_train)
 
 # Predicting a new result
-transformed_array = poly_reg.fit_transform(X_test)
-
-predictions = regressor.predict(transformed_array)
-score = regressor.score(transformed_array, y_test)
-
+predictions = regressor.predict(X_test)
+score = regressor.score(X_test, y_test)
 
 # Only for one dimensional matrix
-def show_plot(X, y, regressor, poly_reg):
+def show_plot(X, y, regressor):
     X_grid = np.arange(min(X), max(X), 0.1)
     X_grid = X_grid.reshape((len(X_grid), 1))
     plt.scatter(X, y, color = 'red')
-    plt.plot(X_grid, regressor.predict(poly_reg.fit_transform(X_grid)), color = 'blue')
-    plt.title('Stock market polynomial regression')
+    plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+    plt.title('Stock market Decision Tree Regression')
     plt.xlabel('Date')
     plt.ylabel('Price')
     plt.show()
     
 # Visualising the train results
-show_plot(X_train, y_train, regressor, poly_reg)
+show_plot(X_train, y_train, regressor)
 
 # Visualising the test results
-show_plot(X_test, y_test, regressor, poly_reg)
+show_plot(X_test, y_test, regressor)
--- regression/svr.py
+++ regression/svr.py
@@ -1,4 +1,4 @@
-# POLYNOMIAL REGRESSION
+# SVR
 
 # Importing the libraries
 import quandl
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 import math
 
 # Parameters
-polynomial_degree = 4
 test_size = 0.2
 quandl_dataset = "WIKI/FB"
 
@@ -20,6 +19,13 @@ dataset['Date'] = (dataset['Date'] - dataset['Date'].min())  / np.timedelta64(1,
 X = dataset.iloc[:, 0:1].values
 y = dataset.iloc[:, 4].values
 
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+sc_y = StandardScaler()
+X = sc_X.fit_transform(X)
+y = sc_y.fit_transform(y)
+
 # Splitting the dataset into the Training set and Test set
 split_index = int(math.floor(len(X) * (1 - test_size)))
 X_train = X[0:split_index, :]
@@ -27,34 +33,26 @@ y_train = y[0:split_index]
 X_test = X[split_index:, :]
 y_test = y[split_index:]
 
-# Fitting to the dataset
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import PolynomialFeatures
-poly_reg = PolynomialFeatures(degree = polynomial_degree)
-X_poly = poly_reg.fit_transform(X_train)
-regressor = LinearRegression()
-regressor.fit(X_poly, y_train)
+# Fitting the SVR to the dataset
+from sklearn.svm import SVR
+regressor = SVR(kernel = 'rbf')
+regressor.fit(X_train, y_train)
 
 # Predicting a new result
-transformed_array = poly_reg.fit_transform(X_test)
-
-predictions = regressor.predict(transformed_array)
-score = regressor.score(transformed_array, y_test)
-
+predictions = sc_y.inverse_transform(regressor.predict(X_test))
+score = regressor.score(X_test, y_test)
 
 # Only for one dimensional matrix
-def show_plot(X, y, regressor, poly_reg):
-    X_grid = np.arange(min(X), max(X), 0.1)
-    X_grid = X_grid.reshape((len(X_grid), 1))
+def show_plot(X, y, regressor):
     plt.scatter(X, y, color = 'red')
-    plt.plot(X_grid, regressor.predict(poly_reg.fit_transform(X_grid)), color = 'blue')
-    plt.title('Stock market polynomial regression')
+    plt.plot(X, regressor.predict(X), color = 'blue')
+    plt.title('Stock market SVR')
     plt.xlabel('Date')
     plt.ylabel('Price')
     plt.show()
     
 # Visualising the train results
-show_plot(X_train, y_train, regressor, poly_reg)
+show_plot(X_train, y_train, regressor)
 
 # Visualising the test results
-show_plot(X_test, y_test, regressor, poly_reg)
+show_plot(X_test, y_test, regressor)
