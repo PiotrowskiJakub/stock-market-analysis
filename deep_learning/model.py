8598ae42801bd0aa52878cf30e9b1530adbd7cd6
Jakub Piotrowski <jakub.piotrowski@cern.ch>
2017-02-17 13:45:48 +0100

    Adapt model skeleton to our solution

--- deep_learning/model.py
+++ deep_learning/model.py
@@ -17,28 +17,28 @@ def lazy_property(function):
 class Model:
 
     def __init__(self):
-        self.data = tf.placeholder(tf.float32, [None, 6, 1])  # [BATCHSIZE, SEQLEN]
-        self.target = tf.placeholder(tf.float32, [None, 3]) # [BATCHSIZE, PREDLEN]
-        self.num_hidden = 24
+        self.data = tf.placeholder(tf.float32, [1, 4, 1])  # [batch_size, companies_number * 2, input_dimension]
+        self.target = tf.placeholder(tf.float32, [1, 2]) # [batch_size, companies_number]
+        self.num_hidden = 8
         self.prediction
         self.optimize
         self.error
 
     @lazy_property
     def prediction(self):
-        cell = tf.nn.rnn_cell.LSTMCell(self.num_hidden)
-        val, _ = tf.nn.dynamic_rnn(cell, self.data, dtype=tf.float32)
-        # return val ?
+        cell = tf.contrib.rnn.LSTMCell(self.num_hidden)
+        val, _ = tf.nn.dynamic_rnn(cell, self.data, dtype = tf.float32) # [batch_size, max_time, cell.output_size]
+        val = tf.reshape(val, [-1, self.num_hidden]) # [batch_size * max_time, cell.output_size] - convert to 2D
         weight = tf.Variable(tf.truncated_normal([self.num_hidden, int(self.target.get_shape()[1])]))
         bias = tf.Variable(tf.constant(0.1, shape=[self.target.get_shape()[1]]))
         incoming = tf.matmul(val, weight) + bias
-        return tf.nn.softmax(incoming)
+        return incoming # try with RELU - tf.nn.relu(incoming)
 
     @lazy_property
     def optimize(self):
-        cross_entropy = -tf.reduce_sum(self.target * tf.log(tf.clip_by_value(self.prediction,1e-10,1.0)))
+        loss = tf.nn.l2_loss(tf.sub(self.prediction, self.target))
         optimizer = tf.train.AdamOptimizer()
-        return optimizer.minimize(cross_entropy)
+        return optimizer.minimize(loss)
 
     @lazy_property
     def error(self):
