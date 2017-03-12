24fa06f8f9d212ef1a9e4d6242738e7371c65f43
Marcin Ostrowski <husarz.em@gmail.com>
2017-03-12 14:57:08 +0100

    Companies from nasdaq, start date 2010

--- deep_learning/stocks_reader.py
+++ deep_learning/stocks_reader.py
@@ -8,10 +8,11 @@ quandl.ApiConfig.api_key = 'jPGm5gjF1imaezGU9QMU'
 FORECASTS_NUM = 5   # Number of days to forecast
 CHANGE_THRESHOLD = 0.3
 
-start_date = date(2016, 9, 1)
-end_date = date(2017, 2, 19)
+start_date = date(2010, 1, 1)
+end_date = date(2017, 3, 10)
 
-COMPANIES = ['WIKI/MSFT', 'WIKI/AAPL']
+COMPANIES_SYMBOLS = ["AMAT", "AAPL", "QSII", "CAMP", "IDTI", "LRCX", "MGRC", "MENT", "JKHY", "ADBE", "CERN", "CY", "FISV", "LLTC", "MSFT", "SIGM", "TECD", "PLAB", "MXIM", "CRUS", "DGII", "SYMC", "CSCO", "XLNX", "PRGS", "QCOM", "ZBRA", "EFII", "KOPN", "SPNS", "SNPS", "AVID", "CREE", "INTU", "MCHP", "PRKR", "SANM", "UTEK", "DSPG", "MIND", "SSYS", "VECO", "BRKS", "CTXS", "HLIT", "IVAC", "KFRC", "NATI", "NTAP", "RSYS", "RCII", "ANSS", "CHKP", "CSGS", "KVHI", "PEGA", "SEAC", "SYKE", "TTEC", "VSAT", "YHOO", "OSIS", "POWI", "RMBS", "RNWK", "SYNT", "TTWO", "AMKR", "CTSH", "MANH", "MSTR", "ULTI", "VRSN", "EPAY", "BRCD", "EGAN", "EXTR", "FFIV", "FNSR", "HSII", "IMMR", "INAP", "JCOM", "NTCT", "NVDA", "PCTI", "PRFT", "QUIK", "ACLS", "CCMP", "HSTM", "ISSC", "LPSN", "MRVL", "SLAB", "SPRT", "TTMI", "MOSY", "OMCL", "PDFS", "CPSI", "STX", "SYNA", "VRNT", "CALD", "FORM", "BLKB", "INTX", "MPWR", "UCTT", "BIDU", "SPWR", "CVLT", "FSLR", "GUID", "IPGP", "SNCR", "CAVM", "ENOC", "GLUU", "GSIT", "TYPE", "RBCN", "SMCI", "VRTU", "ERII", "AVGO", "FTNT", "MDSO", "VRSK"]
+COMPANIES = ['WIKI/' + company for company in COMPANIES_SYMBOLS]
 '''
 COMPANIES = [
         'WIKI/MSFT',
