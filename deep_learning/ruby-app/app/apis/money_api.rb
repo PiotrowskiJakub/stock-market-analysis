9a844c6ebd53557ebc581d7cc043daaa3562da25
Marcin Ostrowski <husarz.em@gmail.com>
2017-06-30 18:56:23 +0200

    Start refactoring

---
+++ deep_learning/ruby-app/app/apis/money_api.rb
@@ -0,0 +1,56 @@
+require 'net/http'
+
+class MoneyApi
+  DATA_SOURCE_URL = 'http://www.money.pl/ajax/gielda/finanse/'
+
+  attr_reader :url
+
+  def initialize
+    @url = URI.parse(DATA_SOURCE_URL)
+  end
+
+  def get_data(params)
+    attributes = %w(
+      date
+      net_income
+      zysk_dzialalnosc
+      zysk_brutto
+      zysk_netto
+      przeplyw_net
+      przeplyw_net_dz_op
+      przeplyw_net_dz_inw
+      przeplyw_net_dz_fin
+      aktywa_razem
+      zobowiazania
+      zobowiazania_dlugoterminowe
+      zobowiazania_krotkoterminowe
+      kapital_wlasny
+      kapital_zakladowy
+      liczba_akcji
+      book_value
+      zysk_per_akcja
+      rozwodniona_liczba_akcji
+      rozwodniona_book_value
+      rozwodniony_zysk_per_akcja
+      dywidenda_per_akcja
+    )
+
+    year_data = [{}, {}, {}, {}]
+
+    response = Net::HTTP.post_form(url, params)
+    response_body = Nokogiri::HTML(response.body)
+
+    data_rows = response_body.css('tr:not(.ikony)')
+
+    data_rows.each_with_index do |row, row_index|
+      attribute = attributes[row_index]
+
+      row.children.css('td, th').each_with_index do |item, item_index|
+        year_data[item_index][attribute] = item.text.delete("\n").delete("\t")
+      end
+    end
+    year_data.reject!(&:empty?)
+
+    { data: year_data, next_page?: response_body.css('.rotorR').css('a')[0].attributes['onclick'].value.include?('showFinanceData') }
+  end
+end
--- deep_learning/ruby-app/app/services/data_scraper.rb
+++ deep_learning/ruby-app/app/services/data_scraper.rb
@@ -17,60 +17,28 @@ class DataScraper
     }
   end
 
-  def get_all
+  def lifetime_data
     objects = []
     loop do
       data = get_data
       objects += data[:data]
-      @params[:o] += 4
-      break if !data[:next]
+      next_page!
+      break if !data[:next_page?]
     end
     objects
   end
 
   private
 
+  def next_page!
+    @params[:o] += 4
+  end
+
+  def money_api
+    @_money_api ||= MoneyApi.new
+  end
+
   def get_data
-    attributes = %w(
-      date
-      net_income
-      zysk_dzialalnosc
-      zysk_brutto
-      zysk_netto
-      przeplyw_net
-      przeplyw_net_dz_op
-      przeplyw_net_dz_inw
-      przeplyw_net_dz_fin
-      aktywa_razem
-      zobowiazania
-      zobowiazania_dlugoterminowe
-      zobowiazania_krotkoterminowe
-      kapital_wlasny
-      kapital_zakladowy
-      liczba_akcji
-      book_value
-      zysk_per_akcja
-      rozwodniona_liczba_akcji
-      rozwodniona_book_value
-      rozwodniony_zysk_per_akcja
-      dywidenda_per_akcja
-    )
-
-    objects = [{}, {}, {}, {}]
-
-    response = Net::HTTP.post_form(url, params)
-    response_body = Nokogiri::HTML(response.body)
-
-    rows = response_body.css('tr:not(.ikony)')
-
-    rows.each_with_index do |row, row_index|
-      attribute = attributes[row_index]
-
-      row.children.css('td, th').each_with_index do |item, item_index|
-        objects[item_index][attribute] = item.text.delete("\n").delete("\t")
-      end
-    end
-
-    { data: objects, next: response_body.css('.rotorR').css('a')[0].attributes['onclick'].value.include?('showFinanceData') }
+    money_api.get_data(params)
   end
 end
